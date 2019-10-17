5c712974a07f5c151b2528edab457264b513d4db
Ariel Valle <avallehh@gmail.com>
2019-10-17 19:44:41 -0400

    config

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ var routes = require("./controllers/burgersController");
 app.use(routes);
 
 
-app.listen(PORT, function() {
-    console.log("Listening on port:%s", PORT);
-});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
